bbafb86db0bbc2df09a5ea96ed3461a755219e35
Abhijit Dhara <abhijitdhara21@gmail.com>
2023-06-05 21:23:22 +0530

    ATBS Basics

---
+++ firstprog.py
@@ -0,0 +1,9 @@
+print('Hello World')
+print("What's your name")
+myName = input()
+print("It's good to meet you " + myName)
+print("the length of your name is ", len(myName))
+
+print("Whats you age?")
+myAge = input()
+print("You will be " + str(int(myAge) + 1) + " in an year")
